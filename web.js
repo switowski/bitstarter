a5b0ff7c1a26a775c36c676e8fef3972907551ac
Ubuntu <ubuntu@ip-172-31-25-15.us-west-2.compute.internal>
2013-07-10 18:11:27 +0000

    Now reads from index.html file

--- web.js
+++ web.js
@@ -1,11 +1,13 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require('fs');
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var buffer = fs.readFileSync('index.html');
+  var msg = buffer.toString();
+  response.send(msg);
 });
-
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
